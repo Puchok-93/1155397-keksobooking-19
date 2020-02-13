b1ad06d1fc20c1341365c7b1b7b54b7e6fae1e89
Puchok-93 <paul.puchkov@yandex.ru>
2020-02-13 19:58:39 +0300

    Добавление маркеров объявлений на карту

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 'use strict';
 
-// Массивы
-// Автор
-var USER_AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var map = document.querySelector('.map');
+var markersBlock = map.querySelector('.map__pins');
+var markersTemplate = document.querySelector('#pin').content;
 
-// Предложение
+// Массивы данных
 
-var OFFERS_TITLE = ['ОБъявление о сдаче', 'Сдам помещение', 'Жду гостей'];
-var PRICE = [1000, 2000, 3000, 4000, 5000];
-var TYPE_HOUSE = ['palace', 'flat', 'house', 'bungalo'];
-var ROOMS = [1, 2, 3, 4, 5];
-var QUESTS = [1, 2, 3, 4, 5, 6];
-var CHECK_IN = ['12:00', '13:00', '14:00'];
-var CHECK_OUT = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var DESCRIPTION = ['Свобно, стильно современно', 'Шикарный вид из окна', 'Рядом с центром города'];
+var avatars = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var title = ['Объявление о сдаче', 'Сдам помещение', 'Жду гостей'];
+var price = [1000, 2000, 3000, 4000, 5000];
+var typeHouse = ['palace', 'flat', 'house', 'bungalo'];
+var rooms = [1, 2, 3, 4, 5];
+var guests = [1, 2, 3, 4, 5, 6];
+var checkIn = ['12:00', '13:00', '14:00'];
+var checkOut = ['12:00', '13:00', '14:00'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var description = ['Свобно, стильно современно', 'Шикарный вид из окна', 'Рядом с центром города'];
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var QUANTITY_OFFERS = 8; // Количество выводимых элементов на страницу
 
 // Генерируем случайное число из массива
 var getRandomElement = function (array) {
@@ -22,42 +26,41 @@ var getRandomElement = function (array) {
 };
 
 // Генерируем случайные координаты Х и Y
-var getRandomCoordinateX = function() {
+var getRandomCoordinateX = function () {
   return Math.floor(Math.random() * (750 - 1 + 1)) + 1;
 };
 
-var getRandomCoordinateY = function() {
+var getRandomCoordinateY = function () {
   return Math.floor(Math.random() * (630 - 130 + 1)) + 130;
 };
 
-// Тест
+// Генерируем объекты
 
-var similarWizardTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-
-// Генерируем случайные объекты
-
-var getRandomMark = function () {
+var getRandomOffer = function () {
   var mark = {
+    author: {
+      avatar: 'img/avatars/user' + getRandomElement(avatars) + '.png'
+    },
+
     offer: {
-      title: getRandomElement(OFFERS_TITLE),
+      title: getRandomElement(title),
       adress: getRandomCoordinateX() + ',' + getRandomCoordinateY(),
-      price: getRandomElement(PRICE),
-      type: getRandomElement(TYPE_HOUSE),
-      rooms: getRandomElement(ROOMS),
-      guests: getRandomElement(QUESTS),
-      checkin: getRandomElement(CHECK_IN),
-      checkout: getRandomElement(CHECK_OUT),
-      features: getRandomElement(FEATURES),
-      description: getRandomElement(DESCRIPTION),
+      price: getRandomElement(price),
+      type: getRandomElement(typeHouse),
+      rooms: getRandomElement(rooms),
+      guests: getRandomElement(guests),
+      checkin: getRandomElement(checkIn),
+      checkout: getRandomElement(checkOut),
+      features: getRandomElement(features),
+      description: getRandomElement(description),
+      photos: getRandomElement(photos)
     },
 
     location: {
       x: getRandomCoordinateX(),
       y: getRandomCoordinateY()
     }
-  }
+  };
   return mark;
 };
 
@@ -66,33 +69,36 @@ var getAdvertisingArr = function (number) {
   var advertising = [];
 
   for (var i = 0; i < number; i++) {
-    advertising.push(getRandomMark());
+    advertising.push(getRandomOffer());
   }
 
   return advertising;
 };
 
-var createAdvertising = getAdvertisingArr(8);
-
 // Клонируем маркеры
 
 var renderMarks = function (mark) {
-  var marksElement = marksTemplate.cloneNode(true);
+  var marks = markersTemplate.querySelector('.map__pin').cloneNode(true);
 
-  marksElement.querySelector('.map__pin').
+  marks.querySelector('img').src = mark.author.avatar;
+  marks.querySelector('img').alt = mark.offer.title;
+  marks.style.left = (mark.location.x - 25) + 'px';
+  marks.style.top = (mark.location.y - 25) + 'px';
 
-  return marksElement;
+  return marks;
 };
 
-var getFragment = function () {
+var getFragment = function (array) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < createAdvertising.length; i++) {
-    fragment.appendChild(renderMarks(createAdvertising[i]));
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderMarks(array[i]));
   }
 
   return fragment;
 };
 
+markersBlock.appendChild(getFragment(getAdvertisingArr(QUANTITY_OFFERS)));
 
-var map = document.querySelector('.map--faded');
+// Скрываем приветствие
+
 map.classList.remove('map--faded');
